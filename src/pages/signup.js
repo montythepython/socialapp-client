4c6bb4f192ffaaffef15c06ac5009a71c41feddf
dg0mez <dariogomezperez@gmail.com>
2020-05-03 19:19:18 +0200

    Tut 25 finished

---
+++ src/pages/signup.js
@@ -0,0 +1,185 @@
+import React, { Component } from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import PropTypes from "prop-types";
+import appLogo from "../images/appLogo.png";
+import { Link } from "react-router-dom";
+
+//MUI Stuff
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+
+//Redux stuff
+import { connect } from "react-redux";
+import { signupUser } from "../redux/actions/userActions";
+
+const styles = (theme) => ({
+  ...theme.spreadThis,
+});
+
+class signup extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      confirmPassword: "",
+      handle: "",
+      errors: {},
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.UI.errors) {
+      this.setState({ errors: nextProps.UI.errors });
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const newUserData = {
+      email: this.state.email,
+      password: this.state.password,
+      confirmPassword: this.state.confirmPassword,
+      handle: this.state.handle,
+    };
+    this.props.signupUser(newUserData, this.props.history);
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  render() {
+    const {
+      classes,
+      UI: { loading },
+    } = this.props;
+    const { errors } = this.state;
+
+    return (
+      <Grid container className={classes.form}>
+        <Grid item sm />
+        <Grid item sm>
+          <img src={appLogo} alt="lanaranja" className={classes.image} />
+          <Typography variant="h2" className={classes.pageTitle}>
+            Sign Up
+          </Typography>
+          <form noValidate onSubmit={this.handleSubmit}>
+            <TextField
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
+              className={classes.textField}
+              helperText={errors.email}
+              error={errors.email ? true : false}
+              value={this.state.email}
+              onChange={this.handleChange}
+              fullWidth
+              // InputProps={{
+              //   startAdornment: (
+              //     <InputAdornment position="start">
+              //       <AccountCircle />
+              //     </InputAdornment>
+              //   ),
+              // }}
+            />
+
+            <TextField
+              id="password"
+              name="password"
+              type="password"
+              label="Password"
+              className={classes.textField}
+              helperText={errors.password}
+              error={errors.password ? true : false}
+              value={this.state.password}
+              onChange={this.handleChange}
+              fullWidth
+            />
+
+            <TextField
+              id="confirmPassword"
+              name="confirmPassword"
+              type="confirmPassword"
+              label="Confirm Password"
+              className={classes.textField}
+              helperText={errors.confirmPassword}
+              error={errors.confirmPassword ? true : false}
+              value={this.state.confirmPassword}
+              onChange={this.handleChange}
+              fullWidth
+            />
+
+            <TextField
+              id="handle"
+              name="handle"
+              type="handle"
+              label="Handle"
+              className={classes.textField}
+              helperText={errors.handle}
+              error={errors.handle ? true : false}
+              value={this.state.handle}
+              onChange={this.handleChange}
+              fullWidth
+            />
+
+            {errors.general && (
+              <Typography variant="body2" className={classes.customError}>
+                {errors.general}
+              </Typography>
+            )}
+
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={classes.button}
+              disabled={loading}
+            >
+              Sign Up
+              {loading && (
+                <CircularProgress size={24} className={classes.progress} />
+              )}
+            </Button>
+
+            <br />
+            <small>
+              already have an account ?<Link to="/login">Log In</Link>
+            </small>
+          </form>
+        </Grid>
+        <Grid item sm />
+      </Grid>
+    );
+  }
+}
+
+signup.propTypes = {
+  classes: PropTypes.object.isRequired,
+  user: PropTypes.object.isRequired,
+  UI: PropTypes.object.isRequired,
+  signupUser: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  user: state.user,
+  UI: state.UI,
+});
+
+const mapActionsToProps = {
+  signupUser,
+};
+
+export default connect(
+  mapStateToProps,
+  mapActionsToProps
+)(withStyles(styles)(signup));
---
+++ src/util/theme.js
@@ -0,0 +1,45 @@
+export default {
+  palette: {
+    primary: {
+      light: "#be9c91",
+      main: "#836744",
+      dark: "#5f4339",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ffffff",
+      main: "#efebe9",
+      dark: "#bdb9b7",
+      contrastText: "#000",
+    },
+  },
+
+  //object to be spread
+  spreadThis: {
+    form: {
+      textAlign: "center",
+    },
+    image: {
+      margin: "20px auto",
+      maxWidth: 100,
+    },
+    pageTitle: {
+      margin: "10px auto",
+    },
+    textField: {
+      margin: "10px auto",
+    },
+    button: {
+      margin: "20px auto 5px auto",
+      position: "relative",
+    },
+    customError: {
+      color: "red",
+      fontSize: "0.8rem",
+      marginTop: 10,
+    },
+    progress: {
+      position: "absolute",
+    },
+  },
+};
